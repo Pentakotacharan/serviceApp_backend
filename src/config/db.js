38b29426e282e7b4a22cad4690e04243202f9e50
Prakash9019 <plsprakash2003@gmail.com>
2025-12-16 15:02:50 +0530

    added

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
