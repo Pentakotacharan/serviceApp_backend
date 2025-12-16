71ef1dcbfeab6efbfb9cb0ebe2de804b8ffc9cfb
Prakash9019 <plsprakash2003@gmail.com>
2025-12-16 16:06:30 +0530

    added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,7 +22,7 @@ const loginWithFirebase = async (req, res) => {
         firebaseUid: decoded.uid,
         email: decoded.email || null,
         phone: decoded.phone_number || null,
-        role: client, // role selected later
+        role: "client", // role selected later
       });
     }
 
