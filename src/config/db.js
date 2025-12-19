de55b3701e905ef67bf1d978354ccd3d5aad871c
Prakash9019 <plsprakash2003@gmail.com>
2025-12-19 12:31:44 +0530

    added

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
-    await mongoose.connect("mongodb+srv://plsprakash2003:Surya_2003@cluster0.bpe9m.mongodb.net/Service_app?retryWrites=true&w=majority");
+    await mongoose.connect('mongodb+srv://plsprakash2003:Surya_2003@cluster0.bpe9m.mongodb.net/Service_app?retryWrites=true&w=majority&appName=Cluster0');
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
